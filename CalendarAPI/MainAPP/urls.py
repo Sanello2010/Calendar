ebb70346a233bc642d3ecd457bb250358cc63092
sanello <sanello@gmail.com>
2020-04-13 01:21:37 +0300

    First commit

---
+++ CalendarAPI/MainAPP/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+   # path("showbooks/", views.ShowBooks.as_view(), name="showbooks_url"),
+   # path("showgenres/", views.ShowGenre.as_view(), name="showgenres_url"),
+
+]
